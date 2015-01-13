8716b800ee44ad3d08e87082ea7c165edab88e9b
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2015-01-13 17:20:28 +0200

    initial commit with ace and saga

---
+++ ace/ace-vendor-blobs.mk
@@ -0,0 +1,35 @@
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_COPY_FILES += \
+    vendor/htc/ace/proprietary/bin/awb_camera:system/bin/awb_camera \
+    vendor/htc/ace/proprietary/bin/lsc_camera:system/bin/lsc_camera \
+    vendor/htc/ace/proprietary/bin/rmt_storage:system/bin/rmt_storage \
+    vendor/htc/ace/proprietary/bin/snd3254:system/bin/snd3254 \
+    vendor/htc/ace/proprietary/lib/libchromatix_ov8810_default_video.so:system/lib/libchromatix_ov8810_default_video.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_ov8810_preview.so:system/lib/libchromatix_ov8810_preview.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_ov8830_default_video.so:system/lib/libchromatix_ov8830_default_video.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_ov8830_hfr.so:system/lib/libchromatix_ov8830_hfr.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_ov8830_preview.so:system/lib/libchromatix_ov8830_preview.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_s5k3h1gx_default_video.so:system/lib/libchromatix_s5k3h1gx_default_video.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_s5k3h1gx_preview.so:system/lib/libchromatix_s5k3h1gx_preview.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_s5k3h2yx_default_video.so:system/lib/libchromatix_s5k3h2yx_default_video.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_s5k3h2yx_hfr.so:system/lib/libchromatix_s5k3h2yx_hfr.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_s5k3h2yx_preview.so:system/lib/libchromatix_s5k3h2yx_preview.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_s5k4e5yx_default_video.so:system/lib/libchromatix_s5k4e5yx_default_video.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_s5k4e5yx_hfr.so:system/lib/libchromatix_s5k4e5yx_hfr.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_s5k4e5yx_preview.so:system/lib/libchromatix_s5k4e5yx_preview.so \
+    vendor/htc/ace/proprietary/lib/libgemini.so:system/lib/libgemini.so \
+    vendor/htc/ace/proprietary/lib/libposteffect.so:system/lib/libposteffect.so \
+    vendor/htc/ace/proprietary/lib/hw/gps.spade.so:system/lib/hw/gps.spade.so
---
+++ saga/saga-vendor-blobs.mk
@@ -0,0 +1,30 @@
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Prebuilt libraries that are needed to build open-source libraries
+PRODUCT_COPY_FILES += \
+	vendor/htc/saga/proprietary/lib/libcamera.so:obj/lib/libcamera.so
+
+# All the blobs necessary for saga
+PRODUCT_COPY_FILES += \
+    vendor/htc/saga/proprietary/bin/awb_camera:/system/bin/awb_camera \
+    vendor/htc/saga/proprietary/bin/lsc_camera:/system/bin/lsc_camera \
+    vendor/htc/saga/proprietary/bin/rmt_storage:/system/bin/rmt_storage \
+    vendor/htc/saga/proprietary/bin/snd3254:system/bin/snd3254 \
+    vendor/htc/saga/proprietary/lib/libcamera.so:/system/lib/libcamera.so \
+    vendor/htc/saga/proprietary/lib/libchromatix_s5k4e1gx_default_video.so:/system/lib/libchromatix_s5k4e1gx_default_video.so \
+    vendor/htc/saga/proprietary/lib/libchromatix_s5k4e1gx_preview.so:/system/lib/libchromatix_s5k4e1gx_preview.so \
+    vendor/htc/saga/proprietary/lib/libgemini.so:/system/lib/libgemini.so \
+    vendor/htc/saga/proprietary/lib/libposteffect.so:/system/lib/libposteffect.so \
+    vendor/htc/saga/proprietary/lib/hw/gps.msm7x30.so:/system/lib/hw/gps.msm7x30.so
