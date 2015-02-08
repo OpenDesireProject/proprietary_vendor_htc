e4a942931c32e3aa0346ba751c974c3440cafda9
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2015-02-08 13:39:30 +0200

    saga: use prebuilt gps blob
    No idea why source built won't work for saga and without a device
    I don't really care to figure out. Hopefully saga's blob isn't
    causing slowdowns and log flood even when gps isn't in use.
    
    Change-Id: Id9cf70cc43694e39749145c99b92cc75a7027197

--- saga/saga-vendor-blobs.mk
+++ saga/saga-vendor-blobs.mk
@@ -20,4 +20,5 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/htc/saga/proprietary/lib/libcamera.so:/system/lib/libcamera.so \
     vendor/htc/saga/proprietary/lib/libchromatix_s5k4e1gx_default_video.so:/system/lib/libchromatix_s5k4e1gx_default_video.so \
-    vendor/htc/saga/proprietary/lib/libchromatix_s5k4e1gx_preview.so:/system/lib/libchromatix_s5k4e1gx_preview.so
+    vendor/htc/saga/proprietary/lib/libchromatix_s5k4e1gx_preview.so:/system/lib/libchromatix_s5k4e1gx_preview.so \
+    vendor/htc/saga/proprietary/lib/hw/gps.msm7x30.so:/system/lib/hw/gps.msm7x30.so
